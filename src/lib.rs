53cc72965f5a20155f2013c38c5d150330f17d4b
Anton Eriksson <antoneriksson978@gmail.com>
2023-02-18 01:23:02 +0800

    add serde json serialization and a user struct for get_account

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,117 @@
 use dotenv::dotenv;
+use serde::{Deserialize, Serialize};
 
-pub async fn get_account() -> Result<String, reqwest::Error> {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    id: String,
+    username: String,
+    perfs: Perfs,
+    #[serde(rename = "createdAt")]
+    created_at: i64,
+    disabled: Option<bool>,
+    #[serde(rename = "tosViolation")]
+    tos_violation: Option<bool>,
+    profile: Profile,
+    #[serde(rename = "seenAt")]
+    seen_at: i64,
+    patron: Option<bool>,
+    verified: Option<bool>,
+    #[serde(rename = "playTime")]
+    play_time: PlayTime,
+    title: Option<String>,
+    url: String,
+    playing: Option<String>,
+    #[serde(rename = "completionRate")]
+    completion_rate: Option<i32>,
+    count: Count,
+    streaming: Option<bool>,
+    followable: bool,
+    following: bool,
+    blocking: bool,
+    #[serde(rename = "followsYou")]
+    follows_you: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Perfs {
+    chess960: Option<Perf>,
+    atomic: Option<Perf>,
+    #[serde(rename = "racingKings")]
+    racing_kings: Option<Perf>,
+    #[serde(rename = "ultraBullet")]
+    ultra_bullet: Option<Perf>,
+    blitz: Option<Perf>,
+    #[serde(rename = "kingOfTheHill")]
+    king_of_the_hill: Option<Perf>,
+    bullet: Option<Perf>,
+    correspondence: Option<Perf>,
+    horde: Option<Perf>,
+    puzzle: Option<Perf>,
+    classical: Option<Perf>,
+    rapid: Option<Perf>,
+    storm: Option<Storm>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Perf {
+    games: i32,
+    rating: i32,
+    rd: i32,
+    prog: i32,
+    prov: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Storm {
+    runs: i32,
+    score: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Profile {
+    country: String,
+    location: Option<String>,
+    bio: Option<String>,
+    #[serde(rename = "firstName")]
+    first_name: String,
+    #[serde(rename = "lastName")]
+    last_name: String,
+    #[serde(rename = "fideRating")]
+    fide_rating: Option<i32>,
+    #[serde(rename = "uscfRating")]
+    uscf_rating: Option<i32>,
+    #[serde(rename = "ecfRating")]
+    ecf_rating: Option<i32>,
+    links: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PlayTime {
+    total: i32,
+    tv: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Count {
+    all: i32,
+    rated: i32,
+    ai: i32,
+    draw: i32,
+    #[serde(rename = "drawH")]
+    draw_h: i32,
+    loss: i32,
+    #[serde(rename = "lossH")]
+    loss_h: i32,
+    win: i32,
+    #[serde(rename = "winH")]
+    win_h: i32,
+    bookmark: i32,
+    playing: i32,
+    import: i32,
+    me: i32,
+}
+
+pub async fn get_account() -> Result<User, reqwest::Error> {
     let url = "https://lichess.org/api/account";
     dotenv().ok();
     let token = std::env::var("PERSONAL_ACCESS_TOKEN")
@@ -9,14 +120,16 @@ pub async fn get_account() -> Result<String, reqwest::Error> {
     let auth_header_value = format!("Bearer {}", token);
 
     let response = reqwest::Client::new()
-        .get("https://lichess.org/api/account")
+        .get(url)
         .header(reqwest::header::AUTHORIZATION, auth_header_value)
         .send()
         .await?
         .text()
         .await?;
-
-    Ok(response)
+    println!("{}", response);
+    let user: User =
+        serde_json::from_str(&response).expect("a json object that corresponds with User struct");
+    Ok(user)
 }
 
 pub async fn get_email() -> Result<String, reqwest::Error> {
@@ -47,7 +160,7 @@ mod tests {
     async fn test_get_account() {
         let result = get_account().await;
         assert!(result.is_ok());
-        assert!(result.unwrap().contains("\"id\":\"fr0zenfire\""));
+        assert!(result.unwrap().id.eq("fr0zenfire"));
     }
 
     #[tokio::test]
