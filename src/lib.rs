4c9ec69fb6303df63d8aca4032f5a786d7c4c60f
Anton Eriksson <antoneriksson978@gmail.com>
2023-02-17 23:59:20 +0800

    add get_email

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,32 @@ use dotenv::dotenv;
 
 pub async fn get_account() -> Result<String, reqwest::Error> {
     let url = "https://lichess.org/api/account";
-
     dotenv().ok();
-
     let token = std::env::var("PERSONAL_ACCESS_TOKEN")
         .expect("a valid PERSONAL_ACCESS_TOKEN in a .env file");
 
     let auth_header_value = format!("Bearer {}", token);
 
-    // Send the HTTP request and get the response
+    let response = reqwest::Client::new()
+        .get("https://lichess.org/api/account")
+        .header(reqwest::header::AUTHORIZATION, auth_header_value)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    Ok(response)
+}
+
+pub async fn get_email() -> Result<String, reqwest::Error> {
+    let url = "https://lichess.org/api/account/email";
+
+    dotenv().ok();
+    let token = std::env::var("PERSONAL_ACCESS_TOKEN")
+        .expect("a valid PERSONAL_ACCESS_TOKEN in a .env file");
+
+    let auth_header_value = format!("Bearer {}", token);
+
     let response = reqwest::Client::new()
         .get(url)
         .header(reqwest::header::AUTHORIZATION, auth_header_value)
@@ -31,5 +48,12 @@ mod tests {
         let result = get_account().await;
         assert!(result.is_ok());
         assert!(result.unwrap().contains("\"id\":\"fr0zenfire\""));
+    }
+
+    #[tokio::test]
+    async fn test_get_email() {
+        let result = get_email().await;
+        assert!(result.is_ok());
+        assert!(result.unwrap().contains("antoneriksson978@gmail.com"));
     }
 }
